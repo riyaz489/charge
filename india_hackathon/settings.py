a7bb40040fd9da4d44cb72b101a02f496e4ce283
riyaz489 <riyaz489.rk@gmail.com>
2020-07-21 20:31:18 +0530

    adding sql config file

--- india_hackathon/settings.py
+++ india_hackathon/settings.py
@@ -121,7 +121,7 @@ else:
             'ENGINE': 'django.db.backends.mysql',
             #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
             'OPTIONS': {
-                'read_default_file': '/etc/mysql/my.cnf',
+                'read_default_file': os.path.join(BASE_DIR, 'sql.cnf')
             },
         }
     }
