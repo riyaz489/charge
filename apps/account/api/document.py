e687e99e6a5c10f5c4374e0a1c6c89c508baa385
riyaz489 <riyaz489.rk@gmail.com>
2020-07-24 09:40:36 +0530

    updating elastic search installation method

--- apps/account/api/document.py
+++ apps/account/api/document.py
@@ -3,6 +3,9 @@ from apps.account.models import Account
 from django_elasticsearch_dsl.registries import registry
 
 
+# run this command intially to create or delete old index and create new one for this app in elasticsearch
+# `./manage.py search_index --rebuild`
+
 @registry.register_document
 class BlogUserDocument(Document):
     class Index:
